74f75cad87c360f0239d4d698ae87e1dad5b087b
jichi <jichifly@gmail.com>
2014-04-20 22:01:43 -0600

    save
    --HG--
    branch : user/jichi

---
+++ cpp/libs/detoursutil/detoursutil.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// detoursutil.h
+// 4/20/2014 jichi
+
+namespace detours {
+
+typedef void *address_t; // LPVOID
+typedef const void *const_address_t; // LPCVOID
+
+// Replace old_addr with new_addr, and return the backup of old_addr if succeed or 0 if fails.
+address_t replace(_Inout_ address_t old_addr, _In_ const_address_t new_addr);
+
+// Ignore type checking
+template<typename Ret, typename Old, typename New>
+inline Ret replace(Old old_addr, New new_addr)
+{ return (Ret)replace((address_t)old_addr, (const_address_t)new_addr); }
+
+} // namespace detours
+
+// EOF
