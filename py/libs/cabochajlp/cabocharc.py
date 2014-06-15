8be47f9e88aba43c50dc3cb1127f3ccce3a2cb1a
jichi <jichifly@gmail.com>
2014-06-14 22:04:48 -0500

    save
    --HG--
    branch : user/jichi

--- py/libs/cabochajlp/cabocharc.py
+++ py/libs/cabochajlp/cabocharc.py
@@ -7,25 +7,7 @@ if __name__ == '__main__': # DEBUG
   sys.path.append("..")
 
 import os
-import MeCab
-from sakurakit.skdebug import dprint, dwarn
-
-# http://stackoverflow.com/questions/10299807/mecab-path-parameters-doesnot-accept-whitespace
-def normalizepath(path):
-  """
-  @param  path  unicode
-  @return  str not unicode
-  """
-  if not path:
-    return ''
-  path = path.encode('utf8', errors='ignore')
-  #if not os.path.exists(path):
-  #  return ''
-  if os.name == 'nt' and ' ' in path:
-    path = path.replace('/', os.path.sep) #.lower()
-    import win32api
-    path = win32api.GetShortPathName(path) # eliminate spaces on the path
-  return '' if ' ' in path else path # the path cannont contain spaces
+from sakurakit.skdebug import dwarn
 
 def maketaggerargs(**kwargs):
   if not kwargs:
@@ -33,37 +15,34 @@ def maketaggerargs(**kwargs):
   l = []
   for k,v in kwargs.iteritems():
     if v:
-      if k in ('dicdir', 'rcfile', 'userdic'):
-        v = normalizepath(v)
-      if not v:
-        dwarn("skip path with spaces for %s" % k)
-      else:
-        l.append('--%s %s' % (k, v))
+      #if k in ('dicdir', 'rcfile', 'userdic'):
+      #  v = normalizepath(v)
+      #if not v:
+      #  dwarn("skip path with spaces for %s" % k)
+      #else:
+      l.append('--%s %s' % (k, v))
   return ''.join(l)
 
-def createtagger(args=None):
+def createparser(args=None):
   """
-  @param  args  str not unicode
-  @return  MeCab.Tagger or None
+  @param* args
+  @return  CaboCha.Parser or None
   """
-  try:
-    ret = MeCab.Tagger(args) if args else MeCab.Tagger()
-    ret.parse("") # critical
-    return ret
-  except Exception, e:
-    dwarn(e)
+  import CaboCha
+  try: return CaboCha.Parser() if args is None else CaboCha.Parser(args)
+  except Exception, e: dwarn(e)
 
-TAGGERS = {} # never delete
-def gettagger(**kwargs):
-  """Taggers are cached
+PARSERS = {} # never delete
+def getparser(**kwargs):
+  """Parser is cached
   @param* dicdir  unicode  path
   @param* userdic  unicode  path
   @return  MeCab.Tagger or None
   """
   args = maketaggerargs(**kwargs)
-  ret = TAGGERS.get(args)
+  ret = PARSERS.get(args)
   if not ret:
-    ret = TAGGERS[args] = createtagger(args)
+    ret = PARSERS[args] = createparser(args)
   return ret
 
 # Environment variables
@@ -72,31 +51,22 @@ def setenvrc(path): # unicode  path to dic rcfile
   try:
     if os.name == 'nt':
       path = path.replace('/', os.path.sep)
-    os.environ['MECABRC'] = path
+    os.environ['CABOCHARC'] = path
   except Exception, e: dwarn(e)
 
 def getenvrc(path): # -> unicode
-  return os.environ.get('MECABRC')
+  return os.environ.get('CABOCHARC')
 
 def delenvrc():
-  try: del os.environ['MECABRC']
+  try: del os.environ['CABOCHARC']
   except: pass
 
 if __name__ == '__main__':
-  dicdir = '/opt/local/lib/mecab/dic/ipadic-utf8'
-  dicdir = '/Users/jichi/opt/Visual Novel Reader/Library/Dictionaries/ipadic'
-  dicdir = '/Users/jichi/src/unidic'
-  dicdir = '/opt/local/lib/mecab/dic/naist-jdic-utf8'
-  rcfile = '/Users/jichi/stream/Library/Dictionaries/mecabrc/ipadic.rc'
+  #rcfile = '/opt/local/etc/cabocharc'
+  #rcfile = '/Users/jichi/stream/Library/Dictionaries/cabocharc/ipadic.rc'
+  rcfile = '/Users/jichi/stream/Library/Dictionaries/cabocharc/unidic.rc'
   setenvrc(rcfile)
-  #tagger = gettagger(dicdir=dicdir)
-  tagger = gettagger()
-  print tagger
-
-  #if os.name == 'nt' and hasattr(config, 'ENV_MECABRC'):
-  #  assert os.path.exists(config.ENV_MECABRC), "mecabrc does not exist"
-  #  os.putenv('MECABRC', config.ENV_MECABRC.replace('/', os.path.sep))
-  #else:
-  #  print "initrc:initenv: ignore mecabrc"
+  parser = getparser()
+  print parser
 
 # EOF
