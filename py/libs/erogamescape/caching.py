9a9132c5461e0fe20e82b42228f5286773c0251b
jichi <jichifly@gmail.com>
2014-07-03 00:37:34 -0500

    save
    --HG--
    branch : user/jichi

--- py/libs/erogamescape/caching.py
+++ py/libs/erogamescape/caching.py
@@ -2,19 +2,20 @@
 # caching.py
 # 8/12/2013 jichi
 
-__all__ = ['CachingApi']
+__all__ = ['CachingGameTableApi', 'CachingReviewTableApi']
 
 if __name__ == '__main__': # DEBUG
   import sys
   sys.path.append("..")
 
 from restful.offline import DataCacher
-from api import Api
+from api import GameTableApi, ReviewTableApi
 
-CachingApi = DataCacher(Api, suffix='.html')
+CachingGameTableApi = DataCacher(GameTableApi, suffix='.html')
+CachingReviewTableApi = DataCacher(ReviewTableApi, suffix='.html')
 
 if __name__ == '__main__':
-  api = CachingApi("s:/tmp/scape", expiretime=86400)
+  api = CachingGameApi("s:/tmp/scape", expiretime=86400)
   t = 15986
   q = api.query(t)
   print q
