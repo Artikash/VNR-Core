5ba37a9344cf6b9e9843bfd20b5e77008ee9c0f6
jichi <jichifly@gmail.com>
2014-07-10 11:56:40 -0500

    save
    --HG--
    branch : user/jichi

---
+++ rb/compass/compass.mk
@@ -0,0 +1,19 @@
+# compass.mk
+# 8/8/2013 jichi
+# http://compass-style.org/help/tutorials/command-line/
+
+ifeq ($(OS),Windows_NT)
+  COMPASS = compass.bat
+  YUI = yuicompressor.cmd
+else
+  COMPASS = compass
+  YUI = yuicompressor
+endif
+
+.yui:
+	$(YUI) --type $(TYPE) -o $(OUT) $(IN)
+
+%.min.css: %.css
+	make .yui TYPE=css OUT=$@ IN=$^
+
+# EOF
