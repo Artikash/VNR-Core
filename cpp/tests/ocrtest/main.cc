39dcdcd857a217428caa14fbcdd023d94029e440
jichi <jichifly@gmail.com>
2014-08-11 15:43:49 -0500

    save
    --HG--
    branch : user/jichi

--- cpp/tests/ocrtest/main.cc
+++ cpp/tests/ocrtest/main.cc
@@ -1,20 +1,19 @@
 // main.cc
 // 6/15/2014 jichi
-#include "comm/wintts.h"
+#include "modiocr/modiocr.h"
 #include <qt_windows.h>
+#include <QtCore/QDebug>
 #include <QtCore/QString>
 
 int main()
 {
-  //const wchar_t *voice = L"HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Speech\\Voices\\Tokens\\VW Misaki";
-  const wchar_t *voice = L"HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Speech\\Voices\\Tokens\\CeVIO-\u3055\u3068\u3046\u3055\u3055\u3089";
+  const wchar_t *path = L"test.tiff";
 
   CoInitialize(nullptr);
-  wintts_t *tts = wintts_create();
-  Q_ASSERT(tts);
-  wintts_set_voice(tts, voice);
-  wintts_speak(tts, text);
-  wintts_destroy(tts);
+  if (modiocr_available())
+    modiocr_from_file(path, [](const wchar_t *text) {
+      qDebug() << QString::fromWCharArray(text);
+    });
 
   CoUninitialize();
   return 0;
