80e2546766098f720f00ce59f3c4a340d5b72d42
jichi <jichifly@gmail.com>
2014-09-21 14:48:53 -0600

    save
    --HG--
    branch : user/jichi

--- py/libs/zhszht/zhja.py
+++ py/libs/zhszht/zhja.py
@@ -3,19 +3,31 @@
 # 9/21/2014: jichi
 # See: http://www.jcinfo.net/jp/kanji/
 
-
 #JT = dict(zip(J, T)) # {unicode:unicode}
 #TJ = dict(zip(T, J)) # {unicode:unicode}
 TJ = {
   # Equivalent
+  u"—": u"ー",
+
   u"說": u"説",
   u"你": u"妳",
+  u"团": u"団",
+  u"蔥": u"葱",
+  u"擊": u"撃",
+  u"絕": u"絶",
+  u"佈": u"布",
+  u"誒": u"欸",
+  u"幫": u"幇",
 
   # Inequivalent
   u"嗎": u"麼",
   u"啊": u"あ",
-  #u"爸": ?,
-  #u"哟": ?,
+  u"哟": u"よ",
+  u"吧": u"巴",
+  u"哎": u"欸",
+  u"呃": u"额",
+
+  u"爸": u"巴",
 }
 
 #def zhs2ja(s): # unicode -> unicode
@@ -24,7 +36,7 @@ def zht2ja(t): # unicode -> unicode
   return ''.join((TJ.get(c) or c for c in t))
 
 if __name__ == '__main__':
-  ja = u"爸說嗎"
+  ja = u"渴餓額姐姐弟弟"
   zh = zht2ja(ja)
 
   from PySide.QtCore import *
