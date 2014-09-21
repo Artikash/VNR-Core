a30ef5251639e414f8d859378e836e83dde033b5
jichi <jichifly@gmail.com>
2014-09-21 17:34:58 -0600

    save
    --HG--
    branch : user/jichi

--- py/apps/reader/managers/textman.py
+++ py/apps/reader/managers/textman.py
@@ -1019,6 +1019,7 @@ class TextManager(QObject):
             sub = zht2ja(sub)
         #elif d.language == 'zhs' and lang.startswith('zh'):
         #  sub = zht2zhs(sub)
+        sub = termman.remove_marks(sub)
         self.agentTranslationProcessed.emit(sub, rawHash, role)
 
     d.addAgentText(text, role, needsTranslation=needsTranslation)
