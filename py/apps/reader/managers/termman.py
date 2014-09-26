3e104a8e8cc3fab3425e01717eca3ba87f538039
jichi <jichifly@gmail.com>
2014-09-26 00:58:40 -0600

    save
    --HG--
    branch : user/jichi

--- py/apps/reader/managers/termman.py
+++ py/apps/reader/managers/termman.py
@@ -30,130 +30,36 @@ def _remove_marks(text): return _re_marks.sub('', text)
 
 RE_MACRO = re.compile('{{(.+?)}}')
 
-@Q_Q
-class _TermManager:
+class TermWriter:
 
-  #SAVE_PARAMS = {
-  #  'tts': {'convertsChinese':False, 'marksChanges':False},
-  #  'ocr': {'convertsChinese':False, 'marksChanges':False},
-  #  'origin': {'convertsChinese':False, 'marksChanges':False},
-  #  'source': {'convertsChinese':False, 'marksChanges':False},
-  #
-  #  'target': {'convertsChinese':True, 'marksChanges':True},
-  #}
+  def __init__(self, createTime, terms, gameIds, hentai, marked):
+    self.createTime = createTime # float
+    self.terms = terms # [Term]
+    self.gameIds = gameIds # set(ing gameId)
+    self.hentai = hentai # bool
+    self.marked = marked # bool
 
-  # Cover all term types, but decouple escape into before and after
-  SAVE_TYPES = 'origin', 'source', 'target', 'speech', 'ocr'
-  #SAVE_TYPES = 'escape_before', 'escape_after'
+    #macros = self._queryTermMacros(terms, lang, gameIds, hentai) # {unicode pattern:unicode repl}
 
-  def __init__(self, q):
-    #self.convertsChinese = False
-    self.enabled = True # bool
-    self.hentai = False # bool
+  def isOutdated(self): # -> bool
+    return self.createTime < _TermManager.instance.updateTime
 
-    self.marked = False # bool
-
-    # For saving terms
-    self.updateTime = 0 # float
-
-    self.targetLanguage = 'ja' # str  targetLanguage
-    self.targetLanguages = {} # [str language:float time]
-
-    self.saveMutex = QMutex()
-
-    self.scripts = {} # {unicode key:TranslationScriptManager}  key = lang + type
-
-    t = self.saveTimer = QTimer(q)
-    t.setSingleShot(True)
-    t.setInterval(1000) # wait for 1 seconds for rebuilding
-    t.timeout.connect(self.saveDirtyTerms)
-
-  def rebuildCacheLater(self): self.saveTimer.start()
-
-  def _createScriptManager(self, type, language): # unicode, unicode -> TranslationScriptManager
-    key = type + language
-    ret = self.scripts.get(key)
-    if not ret:
-      ret = self.scripts[key] = TranslationScriptManager()
-    return ret
-
-  def getScriptManager(self, type, language): # unicode, unicode -> TranslationScriptManager
-    return self.scripts.get(type + language)
-
-  #@classmethod
-  #def needsEscape(cls):
-  #  return config.is_asian_language(cls.language)
-
-  def saveDirtyTerms(self):
-    if not self.saveMutex.tryLock():
-      dwarn("retry later due to thread contention")
-      self.rebuildCacheLater()
-      return
-
-    skthreads.runsync(self._saveDirtyTerms, parent=self.q)
-    self.saveMutex.unlock()
-
-  def _saveDirtyTerms(self): # invoked async
-    dprint("enter")
-    saveTime = time()
-
-    if saveTime < self.updateTime:
-      dwarn("leave: cancel saving out-of-date terms")
-      return
-
-    hentai = self.hentai
-    marked = self.marked
-
-    dm = dataman.manager()
-    gameIds = dm.currentGameIds()
-    if gameIds:
-      gameIds = set(gameIds) # in case it is changed during iteration
-    #terms = list(dm.terms()) # in case it is changed during iteration
-    terms = dm.sortedTerms() # not back up to save memory
-
-    dprint("term count = %s" % len(terms))
-
-    langs = self.targetLanguages
-    #for lang,ts in langs.iteritems():
-    for lang,ts in langs.items(): # back up items
-      if ts < self.updateTime: # skip language that does not out of date
-        for type in self.SAVE_TYPES:
-          if saveTime < self.updateTime:
-            dwarn("leave: cancel saving out-of-date terms")
-            return
-
-          ok = self._saveTerms(saveTime, terms, type, lang, gameIds, hentai, marked)
-          #if not ok:
-          #  dwarn("failed to save terms: type = %s, lang = %s" % (type, lang))
-
-        langs[lang] = saveTime
-
-    dprint("leave")
-
-  def _saveTerms(self, saveTime, terms, type, language, gameIds, hentai, marked):
+  def saveTerms(self, path, type, language):
     """This method is invoked from a different thread
-    @param  terms  [Term]
-    @param  saveTime  float
+    @param  path  unicode
     @param  type  str  term type
     @param  language  str  target text language
-    @param  gameIds  set(ing gameId)
-    @param  hentai  bool
-    @param  marked  bool
     @return  bool
     """
-    marksChanges = marked and type in ('target', 'escape_before', 'escape_after')
-    convertsChinese = language == 'zht' and type in ('target', 'escape_before', 'escape_after')
+    marksChanges = self.marked and type in ('target', 'escape_before', 'escape_after') # bool
+    convertsChinese = language == 'zht' and type in ('target', 'escape_before', 'escape_after') # bool
 
-    path = rc.term_path(type, language)
-    dir = os.path.dirname(path)
-    if not os.path.exists(dir):
-      skfileio.makedirs(dir)
     try:
       empty = True
       with open(path, 'w') as f:
-        f.write(self._termHeader(saveTime, type, language, gameIds, hentai, marked))
-        for td in self._iterTermData(terms, type, language, gameIds, hentai):
-          if saveTime < self.updateTime:
+        f.write(self._createHeader(type, language, marksChanges))
+        for td in self._iterTermData(type, language):
+          if self.isOutdated():
             raise Exception("cancel saving out-of-date terms")
           z = convertsChinese and td.language == 'zhs'
           repl = td.text
@@ -173,26 +79,20 @@ class _TermManager:
               (("r\t%s\n" if regex else "\t%s\n") % pattern))
           empty = False
 
-      man = self._createScriptManager(type, language)
-      if not man.isEmpty():
-        man.clear()
-      if not empty and man.loadFile(path):
-        dprint("type = %s, lang = %s, count = %s" % (type, language, man.size()))
+      if not empty:
         return True
+
     except Exception, e:
       dwarn(e)
 
     skfileio.removefile(path) # Remove file when failed
     return False
 
-  @staticmethod
-  def _termHeader(saveTime, type, language, gameIds, hentai, marked):
+  def _createHeader(self, type, language, marked):
     """
-    @param  terms  [Term]
     @param  type  str
     @param  language  str
-    @param  gameIds  set(int gameId)
-    @param  hentai  bool
+    @param  marked  bool
     @return  unicode
     """
     return """\
@@ -202,11 +102,10 @@ class _TermManager:
 # Unix time: %s
 # Options: type = %s, language = %s, hentai = %s, underline = %s, games = (%s)
 #
-""" % (saveTime, type, language, hentai, marked,
-    ','.join(gameIds) if gameIds else 'empty')
+""" % (self.createTime, type, language, self.hentai, marked,
+    ','.join(self.gameIds) if self.gameIds else 'empty')
 
-  @staticmethod
-  def _iterTermData(terms, type, language, gameIds, hentai):
+  def _iterTermData(self, type, language):
     """
     @param  terms  [Term]
     @param  type  str
@@ -216,15 +115,158 @@ class _TermManager:
     @yield  _Term
     """
     type2 = 'name' if type == 'source' and not config.is_asian_language(language) else ''
-    for t in terms:
+    for t in self.terms:
       td = t.d # To improve performance
       if (not td.disabled and not td.deleted and td.pattern # in case pattern is deleted
           and (td.type == type or type2 and td.type == type2)
-          and (not td.special or gameIds and td.gameId and td.gameId in gameIds)
-          and (not td.hentai or hentai)
+          and (not td.special or self.gameIds and td.gameId and td.gameId in self.gameIds)
+          and (not td.hentai or self.hentai)
           and i18n.language_compatible_to(td.language, language)
         ):
         yield td
+
+  def _queryTermMacros(self, language):
+    """
+    @param  terms  [Term]
+    @param  language  str
+    @param  gameIds  set(int gameId)
+    @param  hentai  bool
+    @return  {unicode pattern:unicode repl} not None
+    """
+    ret = {td.pattern:td.text  for td in self._iterTermData('macro', language)}
+    MAX_COUNT = 1000
+    for count in xrange(1, MAX_COUNT):
+      dirty = False
+      for pattern,text in ret.iteritems(): # not iteritems as I will modify ret
+        if text and defs.TERM_MACRO_BEGIN in text:
+          dirty = True
+          ok = False
+          for m in RE_MACRO.finditer(text):
+            macro = m.group(1)
+            repl = ret.get(macro)
+            if repl:
+              text = text.replace("{{%s}}" % macro, repl)
+              ok = True
+            else:
+              dwarn("missing macro", macro, text)
+              ok = False
+              break
+          if ok:
+            ret[pattern] = text
+          else:
+            ret[pattern] = None # delete this pattern
+      if not dirty:
+        break
+    if count == MAX_COUNT - 1:
+      dwarn("recursive macro definition")
+    return {k:v for k,v in ret.iteritems() if v is not None}
+
+@Q_Q
+class _TermManager:
+
+  instance = None # _TermManager  needed for updateTime
+
+  # Cover all term types, but decouple escape into before and after
+  SAVE_TYPES = 'origin', 'source', 'target', 'speech', 'ocr'
+  #SAVE_TYPES = 'escape_before', 'escape_after'
+
+  def __init__(self, q):
+    _TermManager.instance = self
+
+    #self.convertsChinese = False
+    self.enabled = True # bool
+    self.hentai = False # bool
+
+    self.marked = False # bool
+
+    # For saving terms
+    self.updateTime = 0 # float
+
+    self.targetLanguage = 'ja' # str  targetLanguage
+    self.targetLanguages = {} # [str language:float time]
+
+    self.saveMutex = QMutex()
+
+    self.scripts = {} # {unicode key:TranslationScriptManager}  key = lang + type
+
+    t = self.saveTimer = QTimer(q)
+    t.setSingleShot(True)
+    t.setInterval(1000) # wait for 1 seconds for rebuilding
+    t.timeout.connect(self.saveTerms)
+
+  def rebuildCacheLater(self): self.saveTimer.start()
+
+  def _createScriptManager(self, type, language): # unicode, unicode -> TranslationScriptManager
+    key = type + language
+    ret = self.scripts.get(key)
+    if not ret:
+      ret = self.scripts[key] = TranslationScriptManager()
+    return ret
+
+  def getScriptManager(self, type, language): # unicode, unicode -> TranslationScriptManager
+    return self.scripts.get(type + language)
+
+  #@classmethod
+  #def needsEscape(cls):
+  #  return config.is_asian_language(cls.language)
+
+  def saveTerms(self):
+    if not self.saveMutex.tryLock():
+      dwarn("retry later due to thread contention")
+      self.rebuildCacheLater()
+      return
+
+    saveTime = time()
+    skthreads.runsync(partial(
+        self._saveTerms, saveTime),
+        parent=self.q)
+    self.saveMutex.unlock()
+
+  def _saveTerms(self, createTime):
+    """Invoked async
+    @param  createTime  float
+    """
+    dprint("enter")
+
+    dm = dataman.manager()
+
+    gameIds = dm.currentGameIds()
+    if gameIds:
+      gameIds = set(gameIds) # in case it is changed during iteration
+
+    w = TermWriter(
+      createTime=createTime,
+      terms=dm.sortedTerms(), # not back up to save memory
+      gameIds=gameIds,
+      hentai=self.hentai,
+      marked=self.marked,
+    )
+
+    langs = self.targetLanguages
+    #for lang,ts in self.targetLanguages.iteritems():
+
+    for lang,ts in langs.items(): # back up items
+      if ts < self.updateTime: # skip language that does not out of date
+        for type in self.SAVE_TYPES:
+          if w.isOutdated():
+            dwarn("leave: cancel saving out-of-date terms")
+            return
+
+          path = rc.term_path(type, lang) # unicode
+          dir = os.path.dirname(path) # unicode path
+          if not os.path.exists(dir):
+            skfileio.makedirs(dir)
+
+          man = self._createScriptManager(type, lang)
+          if not man.isEmpty():
+            man.clear()
+
+          if w.saveTerms(path, type, lang) and man.loadFile(path):
+            dprint("type = %s, lang = %s, count = %s" % (type, lang, man.size()))
+
+        langs[lang] = createTime
+
+    dprint("leave")
 
   def applyTerms(self, text, type, language):
     """
@@ -318,13 +360,13 @@ class TermManager(QObject):
 
   ## Queries ##
 
-  def filterTerms(self, terms, language):
-    """
-    @param  terms  iterable dataman.Term
-    @param  language  unicode
-    @yield  Term
-    """
-    return self.__d.iterTerms(terms, language)
+  #def filterTerms(self, terms, language):
+  #  """
+  #  @param  terms  iterable dataman.Term
+  #  @param  language  unicode
+  #  @yield  Term
+  #  """
+  #  return self.__d.iterTerms(terms, language)
 
   def applyTargetTerms(self, text, language):
     """
