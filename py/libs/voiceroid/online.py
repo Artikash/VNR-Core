75e723fcce5130d5f5cb9da24e99f924ae08cd43
jichi <jichifly@gmail.com>
2014-10-08 20:41:50 -0600

    save
    --HG--
    branch : user/jichi

---
+++ py/libs/voiceroid/online.py
@@ -0,0 +1,42 @@
+# coding: utf8
+# 10/8/2014 jichi
+
+# 東北ずん子: http://www.ah-soft.com/voiceroid/zunko/
+#
+# Text: 憎しみは憎しみしか生まない。
+#
+# 1. POST http://voice.ai-j.jp/aitalk_2webapi.php [HTTP/1.1 200 OK 1549ms]
+# Referer: http://voice.ai-j.jp/voiceroid2demo_zunko.swf
+# Content-type: application/x-www-form-urlencoded
+# Content-length: 232
+# Request: gain=&delay=&speaker%5Fid=994&pitch=1%2E0&speed=1%2E0&text=%E6%8I6%8E%E3%81%97%E3%81%BF%E3%81%AF%E6%86%8E%E3%81%97%E3%81%BF%E3%81%97%E3%81%8B%E7%94%9F%E3%81%BE%E3%81%AA%E3%81%84%E3%80%82&password=aidemo&username=AIHPDemo&reqid=create
+# Response: voiceFile=http://182.48.0.54/aitalk-ajax/php/tmp/994_20141009112906855325490.mp3
+#
+# 2. GET http://182.48.0.54/aitalk-ajax/php/tmp/994_20141009112906855325490.mp3 [HTTP/1.1 200 OK 854ms]
+# Referer: http://182.48.0.54/aitalk-ajax/php/tmp/994_20141009112906855325490.mp3
+# Request: empty
+# Response: voiceFile=http://182.48.0.54/aitalk-ajax/php/tmp/994_20141009112906855325490.mp3
+#
+# 3. POST http://voice.ai-j.jp/aitalk_2webapi.php [HTTP/1.1 200 OK 187ms]
+# Referer: http://voice.ai-j.jp/voiceroid2demo_zunko.swf
+# Content-type: application/x-www-form-urlencoded
+# Content-length: 118
+# Request: filename=http%3A%2F%2F182%2E48%2E0%2E54%2Faitalk%2Dajax%2Fphp%2Ftmp%2F994%5F20141009112906855325490%2Emp3&reqid=delete
+# Response: empty
+
+# 結月ゆかり: http://www.ah-soft.com/voiceroid/yukari/index.html
+#
+# Text: 憎しみは憎しみしか生まない。
+#
+# 1. POST http://voice.ai-j.jp/aitalk_2webapi.php [HTTP/1.1 200 OK 2625ms]
+# Referer: http://voice.ai-j.jp/voiceroid2demo_yukari.swf
+# Content-type: application/x-www-form-urlencoded
+# Content-length: 230
+# Request: gain=&delay=&speaker%5Fid=2&pitch=1%2E0&speed=1%2E0&text=%E6%86%8E%E3%81%97%E3%81%BF%E3%81%AF%E6%86%8E%E3%81%97%E3%81%BF%E3%81%97%E3%81%8B%E7%94%9F%E3%81%BE%E3%81%AA%E3%81%84%E3%80%82&password=aidemo&username=AIHPDemo&reqid=create
+# Response: voiceFile=http://182.48.0.54/aitalk-ajax/php/tmp/2_201410091136361167072195.mp3
+#
+# 2. GET http://182.48.0.54/aitalk-ajax/php/tmp/2_201410091136361167072195.mp3 [HTTP/1.1 200 OK 2381ms]
+#
+# 3. POST http://voice.ai-j.jp/aitalk_2webapi.php [HTTP/1.1 200 OK 560ms]
+
+# EOF
