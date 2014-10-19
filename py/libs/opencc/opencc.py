6feb08c6bd077c54746f02c29d173eadbd6d0ced
jichi <jichifly@gmail.com>
2014-10-18 23:47:29 -0600

    save
    --HG--
    branch : user/jichi

---
+++ py/libs/opencc/opencc.py
@@ -0,0 +1,28 @@
+# coding: utf8
+# opencc/__init__.py
+# 10/18/2014 jichi
+
+# Initialization
+
+def init(path):
+  pass
+
+# Conversion
+
+def zh2zht(text): pass
+def zh2zhs(text): pass
+def zh2tw(text): pass
+def zh2hk(text): pass
+def zh2ja(text): pass
+
+def zhs2zht(text): pass
+def zhs2tw(text): pass
+def zhs2hk(text): pass
+def zhs2ja(text): pass
+
+def zht2zhs(text): pass
+def zht2tw(text): pass
+def zht2hk(text): pass
+def zht2ja(text): pass
+
+# EOF
