826e1ef007ba3cd411af3dd5e5d65405ae44a9af
jichi <jichifly@gmail.com>
2014-10-19 10:49:45 -0600

    save
    --HG--
    branch : user/jichi

--- py/libs/opencc/opencc.py
+++ py/libs/opencc/opencc.py
@@ -11,6 +11,14 @@ TYPE_TW = 3
 TYPE_HK = 4
 TYPE_JA = 5
 
+DIC_NAMES = { # {(int fr,int to):str}
+  (TYPE_ZHS, TYPE_ZHT): "STCharacters.txt",
+  (TYPE_ZHS, TYPE_ZHT): "TSCharacters.txt",
+  (TYPE_ZHT, TYPE_TW):  "TWVariants.txt",
+  (TYPE_ZHT, TYPE_HK):  "HKVariants.txt",
+  (TYPE_ZHT, TYPE_JA):  "JPVariants.txt",
+}
+
 # Initialization
 
 OPENCC_DICDIR = "" # unicode  directory of OpenCC's plain-text dictionaries
@@ -40,11 +48,7 @@ def makeconverter(fr, to):
   """
   from pycc import SimpleChineseConverter
   ret = SimpleChineseConverter()
-  txt = ''
-  if fr == TYPE_ZHS and to == TYPE_ZHT:
-    txt = "STCharacters.txt"
-  elif fr == TYPE_ZHS and to == TYPE_ZHT:
-    txt = "TSCharacters.txt"
+  txt = DIC_NAMES.get((fr, to))
   if txt:
     path = os.path.join(OPENCC_DICDIR, txt)
     if os.path.exists(path):
@@ -64,20 +68,22 @@ def convert(text, fr, to):
   """
   return getconverter(fr, to).convert(text)
 
-def zh2zht(text): pass
-def zh2zhs(text): pass
-def zh2tw(text): pass
-def zh2hk(text): pass
-def zh2ja(text): pass
+def zht2zhs(text): return convert(text, TYPE_ZHT, TYPE_ZHS)
+def zht2tw(text): return convert(text, TYPE_ZHT, TYPE_TW)
+def zht2hk(text): return convert(text, TYPE_ZHT, TYPE_HK)
+def zht2ja(text): return convert(text, TYPE_ZHT, TYPE_JA)
 
 def zhs2zht(text): return convert(text, TYPE_ZHS, TYPE_ZHT)
-def zhs2tw(text): pass
-def zhs2hk(text): pass
-def zhs2ja(text): pass
 
-def zht2zhs(text): return convert(text, TYPE_ZHT, TYPE_ZHS)
-def zht2tw(text): pass
-def zht2hk(text): pass
-def zht2ja(text): pass
+# Following are for convenient usage
+def zhs2tw(text): return zht2hw(zhs2zht(text))
+def zhs2hk(text): return zht2hk(zhs2zht(text))
+def zhs2ja(text): return zht2ja(zhs2zht(text))
+
+zh2zht = zhs2zht
+zh2zhs = zht2zhs
+zh2tw = zhs2tw
+zh2hk = zhs2hk
+zh2ja = zhs2ja
 
 # EOF
