7c379e967460e14149bb83c48c4bfa8e02e9c5fc
jichi <jichifly@gmail.com>
2015-01-03 00:25:25 -0700

    save
    --HG--
    branch : user/jichi

--- py/libs/proxyrequests/proxyconfig.py
+++ py/libs/proxyrequests/proxyconfig.py
@@ -2,13 +2,13 @@
 # proxyconfig.py
 # 1/2/2015 jichi
 
-WEBPROXY_CONFIG = {
+WEBPROXY_CONFIG = { # faster, but jittered
   'host': 'http://webproxy.nu',
   'referer': 'http://webproxy.nu',
   'postkey': 'x',
 }
 
-WEBSERVER_CONFIG = {
+WEBSERVER_CONFIG = { # slower, but not jittered
   'host': 'http://web-server.se',
   'referer': 'http://web-server.se',
   'postkey': 'q',
