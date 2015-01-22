f93b3f5c9033296a4a4cf799aede8308f386d289
jichi <jichifly@gmail.com>
2015-01-22 11:51:03 -0700

    save
    --HG--
    branch : user/jichi

--- py/libs/naver/livedoortrans.py
+++ py/libs/naver/livedoortrans.py
@@ -1,8 +1,15 @@
 # coding: utf8
-# iciba.py
-# 8/7/2014 jichi
-# See: jp.iciba.com
-# Only en-ja-zhs are supported.
+# livedoortrans.py
+# 1/22/2015 jichi
+# The service will be shutdown on 1/29/2015
+#
+# Japanese and multilingual: http://livedoor-translate.naver.jp
+# Korean only: http://translate.naver.com
+#
+# Example:
+# API: http://livedoor-translate.naver.jp/text/
+# Request: m=ajaxExecute&translateParams.slang=ja&translateParams.tlang=ko&translateParams.originalText=%E3%81%93%E3%82%93%E3%81%AB%E3%81%A1%E3%81%AF&translateParams.langDetect=Y
+# Response: {"translateStatus":true,"errorMsg":"","translateJsonResult":"[{\"langList\":[\"ja\"],\"tLang\":\"ko\",\"sLang\":\"ja\",\"originalText\":\"こんにちは\",\"locationInfor\":[{\"transTextLen\":5,\"orgTextLen\":5,\"orgTextPos\":0,\"transTextPos\":0}],\"translatedText\":\"안녕하세요\"},{\"tLang\":\"ja\",\"sLang\":\"ko\",\"originalText\":\"안녕하세요\",\"locationInfor\":[{\"transTextLen\":5,\"orgTextLen\":5,\"orgTextPos\":0,\"transTextPos\":0}],\"translatedText\":\"こんにちは\"}]"}
 
 if __name__ == '__main__':
   import sys
@@ -11,35 +18,11 @@ if __name__ == '__main__':
 import json
 import requests
 from sakurakit.skdebug import dwarn, derror
+from sakurakit.sknetio import GZIP_HEADERS
 
 session = requests # global session
 
-# Either of the two are OK.
-# But the difault type are different
-API = "http://jp.iciba.com/api.php"
-#API = "http://fy.iciba.com/api.php"
-
-# Coffeescript extracted from the HTML:
-#  _query = ->
-#    return false  if jQuery.trim(jQuery("#inputC").val()).length is 0
-#    jQuery("#copy-botton").css "display", "none"
-#    jQuery.ajax
-#      type: "post"
-#      dataType: "json"
-#      url: "api.php"
-#      data: "q=" + encodeURIComponent(jQuery(".inputC").val()) + "&type=" + jQuery.mod
-#      success: (msg) ->
-#
-# Request: q=%E4%BD%A0%E5%A5%BD&type=zh-jp
-# Response: {"error":0,"outstr":0,"from":"jp","to":"zh","type":"\u65e5 -> \u4e2d","ret":"\u4f60\u597d\u3002<br \/><br \/>","retcopy":"\u4f60\u597d\u3002"}
-
-_LANG = {
-  'en': 'en',
-  'ja': 'jp',
-  'zhs': 'zh',
-  'zht': 'zh',
-}
-def _lang(lang): return _LANG.get(lang) or 'en' # str -> str
+NAVER_API = "http://livedoor-translate.naver.jp/text/"
 
 def translate(text, to='zhs', fr='ja'):
   """Return translated text, which is NOT in unicode format
@@ -48,20 +31,33 @@ def translate(text, to='zhs', fr='ja'):
   @param  to  unicode not None, must be valid language code
   @return  unicode or None
   """
+  #tok = self.__d.token
+  #if tok:
   try:
-    r = session.post(API, data={
-      'q': text,
-      'type': '-'.join((
-        _lang(fr),
-        _lang(to),
-      ))
-    })
+    r = session.post(NAVER_API,
+      #headers=HEADERS,
+      data={
+        'm': 'ajaxExecute',
+        'translateParams.slang': fr[:2],
+        'translateParams.tlang': to[:2],
+        #'translateParams.langDetect': 'Y',
+        'translateParams.originalText': text,
+      }
+    )
 
-    ret = r.content # content is "err" when failed
+    ret = r.content
     if r.ok and len(ret) > 20 and ret[0] == '{' and ret[-1] == '}':
       #ret = ret.decode('utf8')
-      js = json.loads(ret)
-      return js['retcopy']
+      data = json.loads(ret)
+      #if data.get('translateStatus'): # supposed to be true
+      data = data['translateJsonResult']
+      l = json.loads(data)
+      #print json.dumps(data, sort_keys=True, indent=4)
+      #if len(l) == 1:
+      ret = l[0]['translatedText']
+      #else:
+      #  ret = '\n'.join(it['translatedText'] for it in l)
+      return ret
 
   #except socket.error, e:
   #  dwarn("socket error", e.args)
@@ -80,18 +76,14 @@ def translate(text, to='zhs', fr='ja'):
   except: pass
 
 if __name__ == "__main__":
-  #s = u"ユウキに連れられ、オレは倉屋敷社長の部屋に。"
-  #t = translate(s, to='zhs', fr='ja')
-  #print t
 
   def test():
     global session
 
     #s = u"オープニングやエンディングのアニメーションは単純に主人公を入れ替えた程度の物ではなく、タイトルロゴはもちろん金時や定春の行動や表情、登場する道具（万事屋の面々が乗る車のデザインなど）やクレジット文字など、細部に渡って変更がなされた。更に、坂田金時が『銀魂'』を最終回に追い込み新しいアニメ『まんたま』を始めようとした時にはエンディングや提供表示の煽りコメントが最終回を思わせる演出となり、『まんたま』でも専用のタイトルロゴとオープニングアニメーション（スタッフクレジット付き）が新造され、偽物の提供クレジットまで表示されるなど随所に至るまで徹底的な演出が行われた。また、テレビ欄では金魂篇終了回は『金魂'』最終回として、その翌週は新番組「銀魂'」として案内された。"
-    s = u"ユウキに連れられ、オレは倉屋敷社長の部屋に。"
-
-    fr = 'ja'
-    to = 'zhs'
+    s = u"お花の匂い"
+    fr = "ja"
+    to = "zhs"
 
     #s = u"What are you doing?"
     #fr = "en"
@@ -102,15 +94,15 @@ if __name__ == "__main__":
     from PySide.QtNetwork import QNetworkAccessManager
     session = qtrequests.Session(QNetworkAccessManager())
     with SkProfiler():
-      for i in range(10):
+      for i in range(1):
         t = translate(s, to=to, fr=fr)
-    #print t
+    print t
 
     session = requests.Session()
     with SkProfiler():
-      for i in range(10):
+      for i in range(1):
         t = translate(s, to=to, fr=fr)
-    #print t
+    print t
 
     #session = requests
     #with SkProfiler():
