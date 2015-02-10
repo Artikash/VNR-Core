c14a5f9c7d4a8a64e8751b7512f034a7fb7536aa
jichi <jichifly@gmail.com>
2015-02-09 21:54:57 -0700

    save
    --HG--
    branch : user/jichi

---
+++ py/libs/atlas/atlasdef.py
@@ -0,0 +1,12 @@
+# coding: utf8
+# 2/9/2015
+
+def mt_lang_test(to, fr='ja'):
+  """
+  @param  to  str  language
+  @param* fr  str  language
+  @return  bool
+  """
+  return (fr, to) == ('ja', 'en')
+
+# EOF
