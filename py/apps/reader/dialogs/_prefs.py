89a1673c29ac6743d19f3466b10c10d1c444d0d7
jichi <jichifly@gmail.com>
2015-02-19 02:37:43 -0700

    save
    --HG--
    branch : user/jichi

--- py/apps/reader/dialogs/_prefs.py
+++ py/apps/reader/dialogs/_prefs.py
@@ -3328,7 +3328,7 @@ class _MachineTranslationTab(object):
       my.tr("Japanese-Chinese only"),
       my.tr("experimental"),
     ))
-    ret.setStyleSheet("QCheckBox{color:darkgreen}")
+    ret.setStyleSheet("QCheckBox{color:brown}")
     ret.setChecked(settings.global_().isVTransEnabled())
     ret.toggled.connect(settings.global_().setVTransEnabled)
     return ret
