90b5ae4a1ab554c0da9e73fb3db4abe8a3e30fdb
jichi <jichifly@gmail.com>
2015-05-20 09:40:02 -0600

    save
    --HG--
    branch : user/jichi

--- py/libs/janovp/janovutil.py
+++ py/libs/janovp/janovutil.py
@@ -11,11 +11,11 @@ import janovdef as defs
 # At most 16 characters
 #__name_re = re.compile(ur'【(.{0,%s}?)】|(.{0,%s}?)「'
 #    % (defs.MAX_NAME_LENGTH, defs.MAX_NAME_LENGTH))
-def guess_text_name(text, limit=defs.MAX_NAME_LENGTH, brackets=defs.NAME_BRACKETS):
+def guess_text_name(text, limit=defs.MAX_NAME_LENGTH, quotes=defs.NAME_QUOTES):
   """
   @param  text  unicode
   @param* limit  int
-  @param* brackets  [unicode]
+  @param* quotes  [unicode]
   @return  unicode or None
   """
   if text:
@@ -26,11 +26,11 @@ def guess_text_name(text, limit=defs.MAX_NAME_LENGTH, brackets=defs.NAME_BRACKET
     i = text.find(u'「')
     if 0 < i and i <= limit:
       return text[:i]
-    if brackets:
-      for s in brackets:
-        if text[-1]  == s[-1]:
-          i = text.find(s[0])
-          if 0 < i and i <= limit and -1 == text.find(s[-1], i, -1):
+    if quotes:
+      for q in quotes:
+        if text[-1]  == q[-1]:
+          i = text.find(q[0])
+          if 0 < i and i <= limit and -1 == text.find(q[-1], i, -1):
             return text[:i].strip()
 
   #m = __name_re.match(text)
@@ -39,18 +39,18 @@ def guess_text_name(text, limit=defs.MAX_NAME_LENGTH, brackets=defs.NAME_BRACKET
   #  if r:
   #    return r.strip()
 
-def remove_text_name(text, limit=defs.MAX_NAME_LENGTH, brackets=defs.NAME_BRACKETS):
+def remove_text_name(text, limit=defs.MAX_NAME_LENGTH, quotes=defs.NAME_QUOTES):
   """Hash unicode text combined with context_sep
   @param  text  unicode
   @param* limit  int
-  @param* brackets  [unicode]
+  @param* quotes  [unicode]
   @return   unicode
   """
   if not text:
     return ''
-  for s in brackets:
-    if text[-1] == s[-1]:
-      i = text.find(s[0]) # remove character name
+  for q in quotes:
+    if text[-1] == q[-1]:
+      i = text.find(q[0]) # remove character name
       if i != -1 and i <= limit:
         if i:
           text = text[i:]
