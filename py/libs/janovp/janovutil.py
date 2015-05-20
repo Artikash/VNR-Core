b7bc7870e00c8465480ca65064fa364d7199993a
jichi <jichifly@gmail.com>
2015-05-20 10:22:12 -0600

    save
    --HG--
    branch : user/jichi

--- py/libs/janovp/janovutil.py
+++ py/libs/janovp/janovutil.py
@@ -28,7 +28,7 @@ def guess_text_name(text, limit=defs.MAX_NAME_LENGTH, quotes=defs.NAME_QUOTES):
       return text[:i]
     if quotes:
       for q in quotes:
-        if text[-1]  == q[-1]:
+        if text[-1] == q[-1]:
           i = text.find(q[0])
           if 0 < i and i <= limit and -1 == text.find(q[-1], i, -1):
             return text[:i].strip()
@@ -61,11 +61,71 @@ def remove_text_name(text, limit=defs.MAX_NAME_LENGTH, quotes=defs.NAME_QUOTES):
       text = text[i+1:].lstrip() or text # avoid deleting context
   return text
 
+def split_text_name(text, limit=defs.MAX_NAME_LENGTH, quotes=defs.NAME_QUOTES):
+  """Hash unicode text combined with context_sep
+  @param  text  unicode
+  @param* limit  int
+  @param* quotes  [unicode]
+  @return   (unicode name, unicode text)
+  """
+  if text:
+    i = text.find(u'「')
+    if 0 < i and i <= limit:
+      return text[:i], text[i:]
+    if text[0] == u'【':
+      i = text.find(u'】')
+      if 0 < i and i <= limit:
+        return text[:i+1], text[i+1:]
+    if quotes:
+      for q in quotes:
+        if text[-1] == q[-1]:
+          i = text.find(q[0])
+          if 0 < i and i <= limit and -1 == text.find(q[-1], i, -1):
+            return text[:i], text[i:]
+  return '', text
+
+def split_quotes(text, quotes=defs.NAME_QUOTES):
+  """Hash unicode text combined with context_sep
+  @param  text  unicode
+  @param* quotes  [unicode]
+  @return   [unicode]
+  """
+  left = []
+  right = []
+  if quotes:
+    for q in quotes:
+      while text and text[0] == q[-1]:
+        left.append(text[0])
+        text = text[1:]
+    changed = True
+    while text and changed:
+      changed = False
+      for q in quotes:
+        if text[0] == q[0] and text[-1] == q[-1]:
+          left.append(text[0])
+          right.append(text[-1])
+          text = text[1:-1]
+          changed = True
+  #return left + right
+  if not left:
+    return [text]
+  if text:
+    left.append(text)
+  left.extend(reversed(right))
+  return left
+
 if __name__ == '__main__':
-  t = u'【爽】「悠真くんを攻略すれば２１０円か。なるほどなぁ…」'
+  t = u'【爽】「「悠真くんを攻略すれば２１０円か。なるほどなぁ…」」'
   #t = u'爽（悠真くんを攻略すれば２１０円か。なるほどなぁ…）'
   print guess_text_name(t)
 
   print remove_text_name(t)
+
+  x,y = split_text_name(t)
+  print "x:", x, "y:", y
+
+  y = u'」」' + y
+  for i,it in enumerate(split_quotes(y)):
+    print i,it
 
 # EOF
