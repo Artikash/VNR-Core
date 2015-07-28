c80c4d45b76b9bd067b7dbf6c61adb11abfdd66f
jichi <jichifly@gmail.com>
2015-07-27 21:13:30 -0600

    save
    --HG--
    branch : user/jichi

--- cpp/plugins/vnragent/engine/model/mono.cc
+++ cpp/plugins/vnragent/engine/model/mono.cc
@@ -6,17 +6,18 @@
 #include "engine/enginedef.h"
 #include "engine/enginehash.h"
 #include "engine/engineutil.h"
-#include "util/textutil.h"
+//#include "util/textutil.h"
 //#include "hijack/hijackmanager.h"
 #include "cpputil/cppcstring.h"
 #include "memdbg/memsearch.h"
 #include "mono/monoobject.h"
-#include "mono/monotype.h"
-#include "winhook/hookfun.h"
-#include "winasm/winasmdef.h"
+//#include "mono/monotype.h"
+#include "winhook/hookcode.h"
+//#include "winhook/hookfun.h"
 //#include "ntdll/ntdll.h"
 #include <qt_windows.h>
 #include <cstdint>
+#include <unordered_set>
 
 #define DEBUG "model/mono"
 #include "sakurakit/skdebug.h"
@@ -25,8 +26,8 @@
 
 namespace { // unnamed
 
-mono_object_get_domain_fun_t mono_object_get_domain;
-mono_string_new_utf16_fun_t mono_string_new_utf16;
+//mono_object_get_domain_fun_t mono_object_get_domain;
+//mono_string_new_utf16_fun_t mono_string_new_utf16;
 
 namespace MonoHook {
 namespace Private {
@@ -45,35 +46,74 @@ namespace Private {
   //  return ret;
   //}
 
-  mono_string_to_utf8_fun_t old_mono_string_to_utf8;
-
   bool skipsText(const wchar_t *s, size_t size)
-  { return Util::allAscii(s, size) || ::cpp_wcsnchr(s, size, '/'); }
+  { return size <= 1 || s[0] <= 127 || s[size - 1] <= 127 || ::cpp_wcsnchr(s, size, '/'); }
 
-  char *new_mono_string_to_utf8(MonoString *s)
+  bool before_mono_string(winhook::hook_stack *s)
   {
-    if (s && !skipsText(s->chars, s->length)) {
-      enum { role = Engine::OtherRole, sig = Engine::OtherThreadSignature };
-      //auto split = (ulong)_ReturnAddress();
-      //auto sig = Engine::hashThreadSignature(role, split);
+    std::unordered_set<qint64> hashes_;
+    auto p = (MonoString *)s->stack[1]; // string in arg1
 
-      QString oldText = QString::fromUtf16(s->chars, s->length),
-              newText = EngineController::instance()->dispatchTextW(oldText, role, sig);
-      if (!newText.isEmpty() && newText != oldText) {
-        MonoDomain *domain = mono_object_get_domain(&s->object);
-        MonoString *t = mono_string_new_utf16(domain, newText.utf16(), newText.size());
-        return old_mono_string_to_utf8(t);
-      }
+    if (skipsText(p->chars, p->length) || hashes_.find(Engine::hashWCharArray(p->chars, p->length)) != hashes_.end())
+      return true;
+
+    auto role = Engine::OtherRole;
+
+    // Sample game: Custom Maid 3D 2
+    // TODO: Deal with split logic for different game
+    auto split = s->ecx;
+    if (Engine::isAddressReadable(split)) {
+      split = *(DWORD *)split;
+      if (split == 3 && p->length > 2) // do not treat two kanji as scenario
+        role = Engine::ScenarioRole;
     }
-    return old_mono_string_to_utf8(s);
+
+    auto sig = Engine::hashThreadSignature(role, split);
+    QString oldText = QString::fromUtf16(p->chars, p->length),
+            newText = EngineController::instance()->dispatchTextW(oldText, role, sig);
+    if (!newText.isEmpty() && newText != oldText) {
+      if (newText.size() > oldText.size())
+        newText = newText.left(oldText.size());
+      else {
+        hashes_.insert(Engine::hashString(newText));
+        while (newText.size() < oldText.size())
+          newText.push_back(' ');
+      }
+      newText.toWCharArray(p->chars);
+      hashes_.insert(Engine::hashWCharArray(p->chars, p->length));
+    }
+    return true;
   }
+
+  //mono_string_to_utf8_fun_t old_mono_string_to_utf8;
+  //char *new_mono_string_to_utf8(MonoString *s)
+  //{
+  //  if (s && !skipsText(s->chars, s->length)) {
+  //    enum { role = Engine::OtherRole, sig = Engine::OtherThreadSignature };
+  //    //auto split = (ulong)_ReturnAddress();
+  //    //auto sig = Engine::hashThreadSignature(role, split);
+  //
+  //    QString oldText = QString::fromUtf16(s->chars, s->length),
+  //            newText = EngineController::instance()->dispatchTextW(oldText, role, sig);
+  //    if (!newText.isEmpty() && newText != oldText) {
+  //      MonoDomain *domain = mono_object_get_domain(&s->object);
+  //      MonoString *t = mono_string_new_utf16(domain, newText.utf16(), newText.size());
+  //      t->object = s->object;
+  //      auto ret = old_mono_string_to_utf8(t);
+  //      s->object = t->object;
+  //      return ret;
+  //    }
+  //  }
+  //  return old_mono_string_to_utf8(s);
+  //}
 
 } // namespace Private
 
 bool attach()
 {
   ulong fun = Engine::getModuleFunction("mono.dll", "mono_string_to_utf8");
-  return fun && (Private::old_mono_string_to_utf8 = (mono_string_to_utf8_fun_t)winhook::replace_fun(fun, (ulong)Private::new_mono_string_to_utf8));
+  return fun && winhook::hook_before(fun, Private::before_mono_string);
+  //return fun && (Private::old_mono_string_to_utf8 = (mono_string_to_utf8_fun_t)winhook::replace_fun(fun, (ulong)Private::new_mono_string_to_utf8));
 }
 
 } // namespace MonoHook
@@ -85,8 +125,8 @@ bool MonoEngine::match() { return ::GetModuleHandleA("mono.dll"); }
 
 bool MonoEngine::attach()
 {
-  mono_object_get_domain = (mono_object_get_domain_fun_t)Engine::getModuleFunction("mono.dll", "mono_object_get_domain");
-  mono_string_new_utf16 = (mono_string_new_utf16_fun_t)Engine::getModuleFunction("mono.dll", "mono_string_new_utf16");
+  //mono_object_get_domain = (mono_object_get_domain_fun_t)Engine::getModuleFunction("mono.dll", "mono_object_get_domain");
+  //mono_string_new_utf16 = (mono_string_new_utf16_fun_t)Engine::getModuleFunction("mono.dll", "mono_string_new_utf16");
 
   return MonoHook::attach();
 }
